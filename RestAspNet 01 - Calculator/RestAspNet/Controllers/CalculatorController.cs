0a579508064c8c74c1ddbe63b96048c4ec90f636
Amanda Queiroz Nunes <amanda.queiroz-@hotmail.com>
2020-07-18 18:54:30 -0300

    Implementing Calculator Rest

---
+++ RestAspNet 01 - Calculator/RestAspNet/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAspNet.Controllers
+{
+	[Route("api/[controller]")]
+	public class CalculatorController : Controller
+	{
+
+		// GET api/values/5
+		[HttpGet("{firstNumber}/{secondNumber}")]
+		public IActionResult Sum(String firstNumber, String secondNumber)
+		{
+			if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+			{
+				var sum = Convert.ToInt32(firstNumber) + Convert.ToInt32(secondNumber);
+				return Ok(sum.ToString());
+			}
+			return BadRequest("Invalid Input");
+		}
+
+		private bool IsNumeric(string strNumber)
+		{
+			double number;
+
+			bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+			return isNumber;
+		}
+	}
+}
