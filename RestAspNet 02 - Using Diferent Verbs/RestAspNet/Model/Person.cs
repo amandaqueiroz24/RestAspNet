9d9513028f76222035eaa78c0bd80357b811695a
Amanda Queiroz Nunes <amanda.queiroz-@hotmail.com>
2020-07-19 23:26:08 -0300

    Mock de dados

---
+++ RestAspNet 02 - Using Diferent Verbs/RestAspNet/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAspNet.Model
+{
+	public class Person
+	{
+		public long Id { get; set; }
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public String Address { get; set; }
+		public String Gender { get; set; }
+		
+	}
+}
---
+++ RestAspNet 02 - Using Diferent Verbs/RestAspNet/Services/Implementation/PersonServiceImpl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestAspNet.Model;
+
+namespace RestAspNet.Services.Implementation
+{
+	public class PersonServiceImpl : IPersonService
+	{
+		private volatile int count;
+
+		public Person Create(Person person)
+		{
+			return person;
+		}
+
+		public void Delete(long id)
+		{
+			
+
+		}
+
+		public List<Person> FindAll()
+		{
+			List<Person> people = new List<Person>();
+			for(int i = 0; i < 8; i++)
+			{
+				Person person = MockPerson(i);
+				people.Add(person);
+			}
+
+			return people;
+		}
+
+		private Person MockPerson(int i)
+		{
+			return new Person
+			{
+				Id = IncrementedAndGet(),
+				FirstName = "Amanda",
+				LastName = "Nunes",
+				Address = "São Paulo",
+				Gender = "Femer"
+
+			};
+		}
+
+		private long IncrementedAndGet()
+		{
+			return Interlocked.Increment(ref count);
+		}
+
+		public Person FindById(long id)
+		{
+			return new Person {
+				Id = 1,
+				FirstName = "Amanda",
+				LastName = "Nunes",
+				Address = "São Paulo",
+				Gender = "Femer"
+				
+			};
+		}
+
+		public Person Update(Person person)
+		{
+			return person;
+		}
+	}
+}
